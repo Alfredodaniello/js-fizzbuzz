06fd621e7e9d6737bb5996d0d2cfebeb10f68007
Alfredodaniello <alfredo_daniello@libero.it>
2024-03-07 15:23:41 +0100

    fix script

--- js/script.js
+++ js/script.js
@@ -27,11 +27,11 @@ for(let x = 1; x <= 100; ++x) {
     if (x % 3 === 0 && x % 5 === 0) {
         console.log("FizzBuzz");
     } else if (x % 3 === 0) {   
-        console.log("Fizz")
+        console.log("Fizz");
     }
      else if (x % 5 === 0) {
-        console.log("Buzz")
+        console.log("Buzz");
     } else {
-        console.log(x)
+        console.log(x);
     }
 }
